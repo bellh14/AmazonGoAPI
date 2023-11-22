2b70273d26040d187daac4880442d731dc093687
bellh14 <haybell965@gmail.com>
2023-11-21 20:18:34 -0600

    init

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/bellh14/AmazonGoAPI/pkg/api"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"POST", "GET"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	router.GET("/api/links.json", api.GetProduct)
+	router.GET("/api/links", api.GetProduct)
+	router.POST("/api/links", api.AddLink)
+	router.Run("localhost:8080")
+
+}
